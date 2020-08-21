6e71a158c2d02eab834cbc25fb17a03196b8af01
NitValen <58904391+NitValen@users.noreply.github.com>
2020-08-21 17:05:11 +0200

    Add files via upload

---
+++ chatterbot_bis/preprocessors.py
@@ -0,0 +1,55 @@
+"""
+Statement pre-processors.
+"""
+def clean_upper_case(statement):
+    """
+    Remove upper case letters and replace them with lower case letters from the statement text.
+    """
+
+    statement.text = (statement.text).lower()
+
+    return statement
+
+
+def clean_whitespace(statement):
+    """
+    Remove any consecutive whitespace characters from the statement text.
+    """
+    import re
+
+    # Replace linebreaks and tabs with spaces
+    statement.text = statement.text.replace('\n', ' ').replace('\r', ' ').replace('\t', ' ')
+
+    # Remove any leeding or trailing whitespace
+    statement.text = statement.text.strip()
+
+    # Remove consecutive spaces
+    statement.text = re.sub(' +', ' ', statement.text)
+
+    return statement
+
+
+def unescape_html(statement):
+    """
+    Convert escaped html characters into unescaped html characters.
+    For example: "&lt;b&gt;" becomes "<b>".
+    """
+    import html
+
+    statement.text = html.unescape(statement.text)
+
+    return statement
+
+
+def convert_to_ascii(statement):
+    """
+    Converts unicode characters to ASCII character equivalents.
+    For example: "på fédéral" becomes "pa federal".
+    """
+    import unicodedata
+
+    text = unicodedata.normalize('NFKD', statement.text)
+    text = text.encode('ascii', 'ignore').decode('utf-8')
+
+    statement.text = str(text)
+    return statement
