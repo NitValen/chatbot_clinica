405c5cfa5d7bbabdcc94f2aafac7c8337896b924
NitValen <58904391+NitValen@users.noreply.github.com>
2020-09-07 11:39:04 +0200

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ chatbot = ChatBot(
         {
             'import_path': 'chatterbot.logic.best_match.BestMatch',
             'statement_comparison_function': 'chatterbot_bis.comparisons.JaccardSimilarity',
-            'language' = 'spanish',
+            'language': 'spanish',
             'default_response': 'Lo siento, pero no entiendo.',
             'maximum_similarity_threshold': 0.90
        }
